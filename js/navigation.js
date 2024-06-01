55281a7755045876b5048971263973d26ad74041
Audrius Sveikauskas <audrius@sveikauskas.dev>
2024-06-01 18:41:49 +0300

    add search field on first load

--- js/navigation.js
+++ js/navigation.js
@@ -38,9 +38,15 @@ const handlePageChange = (page) => {
 
     default:
       fetch("pages/home.html")
-        .then((response) => response.text())
-        .then((data) => {
-          document.getElementById("page").innerHTML = data;
-        });
+          .then((response) => response.text())
+          .then((data) => {
+            document.getElementById("page").innerHTML = data;
+          });
+
+      fetch("components/search.html")
+          .then((response) => response.text())
+          .then((data) => {
+            document.getElementById("search").innerHTML = data;
+          });
   }
 };
